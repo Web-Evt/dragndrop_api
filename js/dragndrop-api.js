2ec253b6e37f8dd2b7b54e694491c33b607c7e90
WebEvt <WebEvt@gmail.com>
2013-08-20 22:52:38 +0300

    added once()

--- js/dragndrop-api.js
+++ js/dragndrop-api.js
@@ -5,35 +5,39 @@
 
 (function ($) {
   Drupal.behaviors.dragndropUploadAPI = {
-    attach: function (context, settings) {
-      $.each(settings.dragndropAPI, function (selector, settings) {
-        var $droppable = $(selector);
-        var dnd;
-        // Check if a droppable is a mirror.
-        if (settings.asMirrorFor) {
-          // Try to get DnD instance.
-          dnd = $(settings.asMirrorFor);
-          // Add an event callback for adding a droppable, because may be
-          // the mirrored droppable does not exist yet, so act when it is
-          // initiated.
-          dnd.one('dnd:init', function () {
-            $(this).DnD().addDroppable($droppable);
-          });
-        }
-        // Otherwise just create a new droppable instance.
-        else {
-          dnd = $droppable.DnD(settings);
-          $droppable.bind('dnd:showErrors', showErrors);
-        }
+    attach: function () {
+      $.each(Drupal.settings.dragndropAPI, function (selector, settings) {
+        $(selector).once('dnd-api', function () {
+          var $droppable = $(this);
+
+          var dnd;
+          // Check if a droppable is a mirror.
+          if (settings.asMirrorFor) {
+            // Try to get DnD instance.
+            dnd = $(settings.asMirrorFor);
+            // Add an event callback for adding a droppable, because may be
+            // the mirrored droppable does not exist yet, so act when it is
+            // initiated.
+            dnd.one('dnd:init', function () {
+              $(this).DnD().addDroppable($droppable);
+            });
+          }
+          // Otherwise just create a new droppable instance.
+          else {
+            dnd = $droppable.DnD(settings);
+            $droppable.bind('dnd:showErrors', showErrors);
+          }
+        });
       });
     },
 
     detach: function (context, settings) {
-      $.each(settings.dragndropAPI, function (selector) {
+      $.each(Drupal.settings.dragndropAPI, function (selector) {
         var $droppable = $(selector);
         var dnd = $droppable.DnD();
         if (dnd) {
           dnd.removeDroppable($droppable);
+          $droppable.removeClass('dnd-api-processed');
         }
       });
     }
